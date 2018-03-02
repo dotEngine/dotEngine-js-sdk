43625d6f8603adda6b9282a3b3d840c98cb523e3
notedit <notedit@gmail.com>
2018-03-02 16:45:53 +0800

    fix

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const app = express();
 app.use(express.static('./'));
 
 app.listen(4001, function () {
-  console.log('Example app listening on port 4001!')
+    console.log('Example app listening on port 4001!\n');
+    console.log('Open http://localhost:4001/');
 })
